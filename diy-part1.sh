81991aa1c2e12b9a13976d61dc2247edf4efaf77
Conan <61743295+theConanEdogawa@users.noreply.github.com>
2021-07-24 00:48:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,6 @@
 
 ########################################
 sed -i 's?cache-min-ttl=3600?cache-min-ttl=60?' package/lean/luci-app-turboacc/root/etc/init.d/turboacc;
-sed -i 's?tls://9.9.9.9?tls://223.5.5.5?' package/lean/luci-app-turboacc/root/etc/init.d/turboacc;
+sed -i 's?tls://9.9.9.9?tls://223.5.5.5 -b tls://223.6.6.6?' package/lean/luci-app-turboacc/root/etc/init.d/turboacc;
 sed -i 's?tls://8.8.8.8?tls://162.14.21.178?' package/lean/luci-app-turboacc/root/etc/init.d/turboacc;
 sed -i 's?"tls://162.14.21.178"?& -f "tls://223.5.5.5" -f "tls://223.6.6.6"?' package/lean/luci-app-turboacc/root/etc/init.d/turboacc
